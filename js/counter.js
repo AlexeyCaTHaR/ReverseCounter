d32865697bff568abebedb8d09b55277b2c1a8ea
AlexeyCaTHaR <alexey@cathar.ru>
2014-06-03 14:55:00 +0400

    Added two digit style
    Added two digit style

--- js/counter.js
+++ js/counter.js
@@ -1,11 +1,11 @@
-function init_counter(container,day,month,year,hour,min,sec) {
+function init_counter(container,day,month,year,hour,min,sec,style) {
 	month = month-1;
 	var end = new Date(year,month,day,hour,min,sec);
 	setInterval(function(){
-		set_time(container,end);
+		set_time(container,end,style);
 	}, 1000);
 }
-function set_time(container,end){
+function set_time(container,end,style){
 	var now = new Date();
 	var diff = end.valueOf()-now.valueOf();
 	if(diff>0){
@@ -20,10 +20,13 @@ function set_time(container,end){
 	}else{
 		var d='00'; var h='00'; var m='00'; var s='00';
 	}
-	
-	$(container).html('<div class="days">'+d+'<div class="sep"></div></div><div class="countText">Дни</div><div class="hours">'+h+'<div class="sep"></div></div><div class="countText">Часы</div><div class="minutes">'+m+'<div class="sep"></div></div><div class="countText">Мин</div><div class="seconds">'+s+'<div class="sep"></div></div><div class="countText">Сек</div>');
+	if(style == 0){
+		$(container).html('<div class="days">'+d+'<div class="sep"></div></div><div class="countText">Дни</div><div class="hours">'+h+'<div class="sep"></div></div><div class="countText">Часы</div><div class="minutes">'+m+'<div class="sep"></div></div><div class="countText">Мин</div><div class="seconds">'+s+'<div class="sep"></div></div><div class="countText">Сек</div>');
+	}else{
+		$(container).html('<div class="digit">'+d.toString().slice(0,1)+'</div><div class="digit">'+d.toString().slice(1,2)+'</div><div class="digitText">Дни</div><div class="digit">'+h.toString().slice(0,1)+'</div><div class="digit">'+h.toString().slice(1,2)+'</div><div class="digitText">Часы</div><div class="digit">'+m.toString().slice(0,1)+'</div><div class="digit">'+m.toString().slice(1,2)+'</div><div class="digitText">Минуты</div><div class="digit">'+s.toString().slice(0,1)+'</div><div class="digit">'+s.toString().slice(1,2)+'</div><div class="digitText">Секунды</div>');
+	}
 }
-function week_counter(container,to){
+function week_counter(container,to,style){
 	var now = new Date();
 	var day = now.getDay();
 	to = to!=7?to:0;
@@ -36,6 +39,6 @@ function week_counter(container,to){
 		var end = new Date(now.getFullYear(),now.getMonth(),now.getDate(),23,59,59,999);
 	}
 	setInterval(function(){
-		set_time(container,end);
+		set_time(container,end,style);
 	}, 1000);
 }
