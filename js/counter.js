f4570363919ad493b417f6eee9f38851ec6278c0
AlexeyCaTHaR <alexey@cathar.ru>
2014-06-03 17:52:00 +0400

    Add some params
    Add hours, minutes and seconds params to function
    week_counter(container,to,h,m,s,style)

--- js/counter.js
+++ js/counter.js
@@ -26,13 +26,13 @@ function set_time(container,end,style){
 		$(container).html('<div class="digit">'+d.toString().slice(0,1)+'</div><div class="digit">'+d.toString().slice(1,2)+'</div><div class="digitText">Дни</div><div class="digit">'+h.toString().slice(0,1)+'</div><div class="digit">'+h.toString().slice(1,2)+'</div><div class="digitText">Часы</div><div class="digit">'+m.toString().slice(0,1)+'</div><div class="digit">'+m.toString().slice(1,2)+'</div><div class="digitText">Минуты</div><div class="digit">'+s.toString().slice(0,1)+'</div><div class="digit">'+s.toString().slice(1,2)+'</div><div class="digitText">Секунды</div>');
 	}
 }
-function week_counter(container,to,style){
+function week_counter(container,to,h,m,s,style){
 	var now = new Date();
 	var day = now.getDay();
 	to = to!=7?to:0;
 	if(day!=to){
-		var tmp = new Date(now.getFullYear(),now.getMonth(),now.getDate(),23,59,59,999);
-		tmp = tmp.valueOf()-now.valueOf();
+		var tmp = new Date(now.getFullYear(),now.getMonth(),now.getDate(),h,m,s,999);
+		tmp = Math.abs(tmp.valueOf()-now.valueOf());
 		tmp = to>day?(tmp + 86400000*(to-day)):(day!=0?(tmp+(7-day+to)*86400000):(tmp+to*86400000));
 		var end = new Date((tmp+now.valueOf()));
 	}else{
